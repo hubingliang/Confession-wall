9c65fb93bb68419d422ab64e0a73b65154666736
brown <1104524352@qq.com>
2017-08-28 23:35:51 +0800

    上传图片功能初步解决

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -4,26 +4,26 @@
         <div class="user">
             <img src="https://i.loli.net/2017/08/19/5997affaf2cda.jpg"/>
             <div class="username">
-                <h4 @click="con()">sdasdas</h4>
+                <h4>{{item.username}}</h4>
                 <span>用无限适用于未来的方法，置换体内星辰河流。</span>
             </div>
         </div>
         <div class="content">
             <img class="img" src="https://i.loli.net/2017/08/19/5997acbb7dd43.jpg"/>
-            <p>我能吞下玻璃而不伤身体，The quick brown fox jumps over the lazy dog.</p>
+            <p>{{item.content}}</p>
         </div>
         <div class="actions">
             <div class="good">
                 <svg class="items-icon" aria-hidden="true">
                     <use xlink:href="#icon-dianzan2"></use>
                 </svg>
-                <span class="number">2333</span>
+                <span class="number">{{item.good}}</span>
             </div>
             <div class="comment-icon">
                 <svg class="items-icon" aria-hidden="true">
                     <use xlink:href="#icon-pinglun"></use>
                 </svg>
-                <span class="comment">2333 条评论</span>
+                <span class="comment">{{item.commentNumber}} 条评论</span>
             </div>
         </div>
         <div class="line"></div>
@@ -39,10 +39,17 @@
 
 <script>
 export default {
-    props:['ConfessionData'],
+    data(){
+        return{
+        }
+    },
+    props:['ConfessionData','item'],
+    mounted() {
+        this.read()
+    },
     methods:{
-        con:function(){
-            console.log(this.ConfessionData)
+        read:function(){
+            console.log(this.item)
         }
     }
 }
