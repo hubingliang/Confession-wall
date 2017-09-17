268527f9b44ecc6f2a267c3320a6dc23124f044e
brown <1104524352@qq.com>
2017-09-18 00:00:12 +0800

    用户文件与表白墙文件分别储存

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -279,7 +279,7 @@ export default {
       var fileUploadControl = $('#userImage')[0];
       if (fileUploadControl.files.length > 0) {
         var localFile = fileUploadControl.files[0];
-        var name = 'avatar.jpg';
+        var name = 'userImage.jpg';
 
         var file = new AV.File(name, localFile);
         file.save().then((file)=> {
